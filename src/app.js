2d51404bfb031c805919ea3547b18fc943847859
darunyat <108018247+darunyat@users.noreply.github.com>
2022-07-23 16:59:57 +0200

    added a search engine

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-let apiKey = "a161492f71b97ed4d827ea73bfed8c93";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dehli&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -53,4 +48,19 @@ function displayTemperature(response) {
   let alt = response.data.weather[0].main;
   iconElement.setAttribute("alt", alt);
 }
-axios.get(apiUrl).then(displayTemperature);
+
+function search(city) {
+  let apiKey = "a161492f71b97ed4d827ea73bfed8c93";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector(`#city-input`);
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector(`#search-form`);
+form.addEventListener("submit", handleSubmit);
+
+search(`Kyiv`);
